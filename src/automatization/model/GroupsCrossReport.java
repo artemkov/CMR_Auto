3ebd5c5ba79fc37f10ed2acb1d1b77b2a82f2748
Артем Ковалев < @DeskTop>
2017-07-13 23:16:18 +0300

    automatization.model.OlgaWeightedReport: Исправлена ошибка с процентами в значимостях automatization.model.DAReport: замена типа параметров конструктора (было NPSReport стало NPSgetter) Исправлен некорректный расчет ConfInt Написание Olga1ReportFactory 15.11.16 Исправлен ряд ошибок 24.11.16 Работа над значимостями средних 26.11.16 Исправлено вычичление значимостей с Бонферрони 26.11.16 Исправлен неправильный рассчет значимостей средних в отчете Olga из-за неучета ExcludeList в расчете общего веса выборки

--- src/automatization/model/GroupsCrossReport.java
+++ src/automatization/model/GroupsCrossReport.java
@@ -186,7 +186,7 @@ public class GroupsCrossReport
             igroup1 = GroupsReport.constructInterviewGroupsFormContent(content1);
             if (igroup1==null)
             {
-                igroup1 = GroupsReport.constructInterviewGroupsFormContent(content1,interviews,true);
+                igroup1 = GroupsReport.constructInterviewGroupsFormContent(content1,interviews,false);
             }
             agrop1fictive = true;
         }
@@ -235,7 +235,7 @@ public class GroupsCrossReport
                 igroup2 = GroupsReport.constructInterviewGroupsFormContent(content2);
                 if (igroup2==null)
                 {
-                    igroup2 = GroupsReport.constructInterviewGroupsFormContent(content2,interviews,true);
+                    igroup2 = GroupsReport.constructInterviewGroupsFormContent(content2,interviews,false);
                 }
                 agrop2fictive = true;
             }
@@ -270,7 +270,7 @@ public class GroupsCrossReport
                 igroup2 = GroupsReport.constructInterviewGroupsFormContent(content2);
                 if (igroup2==null)
                 {
-                    igroup2 = GroupsReport.constructInterviewGroupsFormContent(content2,interviews,true);
+                    igroup2 = GroupsReport.constructInterviewGroupsFormContent(content2,interviews,false);
                 }
                 agrop2fictive = true;
             }
